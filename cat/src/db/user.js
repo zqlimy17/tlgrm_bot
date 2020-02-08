05e504bc64fe7b4c278ec5d366f6dfc446433750
kinskin <md.asshikin@linqcash.com>
2020-02-09 02:15:43 +0800

    added Op

--- cat/src/db/user.js
+++ cat/src/db/user.js
@@ -1,9 +1,53 @@
 const User = require("../services/models/user");
+const DbLogs = require("./log");
+const DbChats = require("./chat");
+const { Op } = require("sequelize");
 
 class DbUsers {
-    static async users() {
-        let users = User.findAll();
-        if (users) return users;
+    async users() {
+        let users = await User.findAll();
+        if (users) {
+            users = users.map(async user => {
+                user = user.get();
+            });
+        }
+        return users;
+    }
+
+    async users_logs() {
+        let users = await User.findAll();
+        if (users) {
+            users = users.map(async user => {
+                let logs = await new DbLogs(user.telegram_id).logs();
+                logs = logs.map(log => {
+                    return log.get();
+                });
+                user = user.get();
+                user[`log_count`] = logs.length;
+                user[`logs`] = logs;
+                return user;
+            });
+            users = await Promise.all(users);
+        }
+        return users;
+    }
+
+    async users_chats() {
+        let users = await User.findAll();
+        if (users) {
+            users = users.map(async user => {
+                let chats = await new DbChats(user.telegram_id).chats();
+                chats = chats.map(chat => {
+                    return chat.get();
+                });
+                user = user.get();
+                user[`chat_count`] = chats.length;
+                user[`chats`] = chats;
+                return user;
+            });
+            users = await Promise.all(users);
+        }
+        return users;
     }
 }
 
--- cat/src/utils/routes.js
+++ cat/src/utils/routes.js
@@ -2,8 +2,26 @@ const express = require("express");
 const router = express.Router();
 const DbUsers = require("../db/user");
 
+router.get("/users/logs", async (req, res) => {
+    let users_logs = await new DbUsers().users_logs();
+    if (users_logs) {
+        res.send({ users_logs }).status(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.get("/users/chats", async (req, res) => {
+    let users_chats = await new DbUsers().users_chats();
+    if (users_chats) {
+        res.send({ users_chats }).status(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
 router.get("/users", async (req, res) => {
-    let users = await DbUsers.users();
+    let users = await new DbUsers().users();
     if (users) {
         res.send({ users }).status(200);
     } else {
