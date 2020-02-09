c4d06a67db01e61a7af0f68a4cdcf9ad0d33fc85
kinskin <md.asshikin@linqcash.com>
2020-02-09 16:44:53 +0800

    added chat_users table

--- bot/src/utils/webhook.js
+++ bot/src/utils/webhook.js
@@ -1,13 +1,13 @@
 const API = require("../services/api");
-const Db = require("../services/db");
-const Message = require("./message");
+const Db = require("../services/db/db");
+const Message = require("../services/db/message");
 
 class Webhooks {
     async filter(payload) {
         if (payload.hasOwnProperty("message")) {
             let { from, chat, text } = payload.message;
-            await new Message(from, chat).additional_props(payload.message);
-            new Db(from, chat, text).processing_data(payload.message.message_id);
+            await new Db(from, chat, text).processing_data(payload.message.message_id);
+            new Message(from, chat).additional_props(payload.message);
             if (chat.type === "private") {
                 try {
                     new API(payload, payload.message.from).delete_message();
