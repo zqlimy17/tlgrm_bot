6a7ba57ba50a155a894b0ff4980be8faea8d2f0d
zqlimy17 <zqlimy@gmail.com>
2020-02-10 11:08:26 +0800

    update

--- app/src/pages/Group.js
+++ app/src/pages/Group.js
@@ -6,20 +6,22 @@ const Group = () => {
     let { id } = useParams();
     const [messages, setMessages] = useState([]);
     const [group, setGroup] = useState();
-    useEffect(() => {
+    useEffect(async () => {
         console.log(`http://localhost:4040/group/${id}`);
-        axios.get(`http://localhost:4040/group/${id}`).then(res => {
-            let reverse = res.data.logs;
-            reverse.reverse();
-            setMessages(reverse);
-            setGroup(res.data.chat);
-            console.log("RES DATA CHAT IS", res.data.chat);
-        });
+        let res = await axios.get(`http://localhost:4040/group/${id}`);
+        let reverse = res.data.logs;
+        reverse.reverse();
+        await setMessages(reverse);
+        await setGroup(res.data.chat);
+        console.log("RES DATA CHAT IS", res.data.chat);
     }, []);
     return (
         <>
-            <h1></h1>
-            {console.log(group)}
+            <h1>{group ? group.chat_name : ""}</h1>
+            {console.log(
+                "messages is: ",
+                messages.length > 0 ? messages[0].created_at : ""
+            )}
             {messages.slice(0, 20).map(message => {
                 return (
                     <div>
