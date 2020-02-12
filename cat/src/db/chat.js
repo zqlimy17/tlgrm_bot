fc532c036bb70db9059b79d37a0b5153bc751217
kinskin <md.asshikin@linqcash.com>
2020-02-12 21:53:58 +0800

    fixed the bugs

--- cat/src/db/chat.js
+++ cat/src/db/chat.js
@@ -1,20 +1,33 @@
 const Chat = require("../services/models/chat");
+const ChatUsers = require("../services/models/chat_users");
 const { Op } = require("sequelize");
 
 class DbChats {
     constructor(telegram_id) {
         this.telegram_id = telegram_id;
     }
+
     async chats() {
         let chats = await Chat.findAll({
             where: {
-                chat_owner: this.telegram_id,
                 chat_type: {
                     [Op.not]: "private"
                 }
             }
         });
-        if (chats) return chats;
+        let chat_users = await ChatUsers.findAll({
+            where: { telegram_id: this.telegram_id }
+        });
+        chat_users = chat_users.map(chat_user => {
+            let chat = chats.filter(e => e.chat_id == chat_user.chat_id);
+            if (chat.length !== 0) {
+                chat = chat[0].get();
+                chat.user_status = chat_user.chat_user_status;
+                return chat;
+            }
+        });
+        chat_users = chat_users.filter(e => !!e);
+        return chat_users;
     }
 
     async chat(chat_id) {
--- cat/src/db/user.js
+++ cat/src/db/user.js
@@ -4,6 +4,23 @@ const DbChats = require("./chat");
 const { Op } = require("sequelize");
 
 class DbUsers {
+    constructor(telegram_id) {
+        this.telegram_id = telegram_id;
+    }
+
+    async user() {
+        let user = await User.findOne({
+            where: {
+                telegram_id: this.telegram_id
+            }
+        });
+        if (user) {
+            user = user.get();
+            return user;
+        }
+        return;
+    }
+
     async users() {
         console.log("working");
         let users = await User.findAll();
